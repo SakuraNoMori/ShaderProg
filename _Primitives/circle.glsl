1bbe72f2ac9230736c3338caa714ca59edb2d621
Shira Nai <clanlessmew@web.de>
2022-03-25 11:07:30 +0100

    Added shader for circle

--- _Primitives/circle.glsl
+++ _Primitives/circle.glsl
@@ -1,3 +1,4 @@
+
 #ifdef GL_ES
 precision mediump float;
 #endif
@@ -6,12 +7,21 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
+float cirlceEmpty(vec2 st,vec2 center,float r,float width)
+{
+	float dist=distance(st,center);
+	return (dist<=r+width/2&&dist>=r-width/2)?1.:0.;
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
     st.x *= u_resolution.x/u_resolution.y;
 
     vec3 color = vec3(0.);
-    color = vec3(st.x,st.y,abs(sin(u_time)));
+	vec2 center=vec2(0.6,0.5);
+	float radius=0.2;
+	float width;
+    color = vec3(cirlceEmpty(st,center,radius,0.15));
 
     gl_FragColor = vec4(color,1.0);
 }
